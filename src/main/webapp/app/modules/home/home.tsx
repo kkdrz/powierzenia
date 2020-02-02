5082c17bd90276688c5bf202ee092cbdac6d9566
Konrad Drozd <drozdkonrad@gmail.com>
2020-02-02 14:54:50 +0100

    Remove some JHipster boilerplate

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -14,7 +14,7 @@ export const Home = (props: IHomeProp) => {
   return (
     <Row>
       <Col md="9">
-        <h2>Welcome, Java Hipster!</h2>
+        <h2>Welcome!</h2>
         <p className="lead">This is your homepage</p>
         {account && account.login ? (
           <div>
@@ -25,7 +25,7 @@ export const Home = (props: IHomeProp) => {
             <Alert color="warning">
               If you want to
               <a href={getLoginUrl()} className="alert-link">
-                sign in
+                <span> sign in</span>
               </a>
               , you can try the default accounts:
               <br/>- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
@@ -35,47 +35,6 @@ export const Home = (props: IHomeProp) => {
             </Alert>
           </div>
         )}
-        <p>If you have any question on JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              JHipster homepage
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster on Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank"
-               rel="noopener noreferrer">
-              JHipster bug tracker
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              JHipster public chat room
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              follow @jhipster on Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          If you like JHipster, do not forget to give us a star on{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
-      </Col>
-      <Col md="3" className="pad">
-        <span className="hipster rounded"/>
       </Col>
     </Row>
   );
