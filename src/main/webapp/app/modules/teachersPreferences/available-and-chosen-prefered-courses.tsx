4e6913245f8070820dbc3bc001728da41f95fe07
Konrad Drozd <drozdkonrad@gmail.com>
2020-02-02 14:10:57 +0100

    Implement preferedCourses in teachersPreferences

--- src/main/webapp/app/modules/teachersPreferences/available-and-chosen-prefered-courses.tsx
+++ src/main/webapp/app/modules/teachersPreferences/available-and-chosen-prefered-courses.tsx
@@ -3,22 +3,49 @@ import './teachers-preferences.scss';
 import React from 'react';
 
 import {connect} from 'react-redux';
-import {Col, Row} from 'reactstrap';
+import {Button, Col, Row} from 'reactstrap';
 
 
 export const AvailableAndChosenPreferedCourses = (props) => {
 
+  const leftColumnStyle = {
+    borderRight: "1px solid black"
+  };
+
+  const onAvailableClick = item => {
+    props.onUpdate([...props.chosen, item]);
+  };
+
+  const onChosenClick = item => {
+    props.onUpdate(props.chosen.filter(i => i.id !== item.id));
+  };
+
   return (
     <Row>
-      <Col sm="6">
+      <Col style={leftColumnStyle} sm="6">
         {props.available.map((item, i) => {
-          return <span key={i}>{item.type}</span>
+          return <Row key={i}>
+            <Col xs="12" className={"mt-1"}>
+              <Button onClick={() => onAvailableClick(item)} block>
+                {item.name}
+              </Button>
+            </Col>
+          </Row>
         })}
       </Col>
       <Col sm="6">
+        <Row><Col><h3>Declared courses</h3></Col></Row>
+
         {props.chosen.map((item, i) => {
-          return {item}
+          return <Row key={i}>
+            <Col xs="12" className={"mt-1"} onClick={() => onChosenClick(item)}>
+              <Button color={"success"} outline block>
+                {item.name}
+              </Button>
+            </Col>
+          </Row>
         })}
+
       </Col>
     </Row>
   );
